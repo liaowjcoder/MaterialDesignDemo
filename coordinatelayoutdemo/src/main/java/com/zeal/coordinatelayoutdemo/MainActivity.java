9ad9bd176b609615acb2d271b0aec8800c1a0f8e
zeal <lwj_zeal@163.com>
2016-11-26 21:11:53 +0800

    Toolbar -- CollapsingToolbarLayout的引入

--- coordinatelayoutdemo/src/main/java/com/zeal/coordinatelayoutdemo/MainActivity.java
+++ coordinatelayoutdemo/src/main/java/com/zeal/coordinatelayoutdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.zeal.coordinatelayoutdemo;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -10,7 +13,7 @@ import android.widget.Toast;
  * activity_main:实现了只有手指往下滑动，标题栏就可以见
  * CoordinateLayout作为跟布局
  * 在布局中指定标题栏显示的Behavior行为-->app:layout_scrollFlags="scroll|enterAlways"
- *
+ * <p>
  * 标记：
  * scroll：若是当前view需要滑动，那么就必须要这个标记,没有设置这个标记将被固定在屏幕底部
  * enterAlways：当滑动组件向下滚动时，标题栏会直接往下滚动。
@@ -18,16 +21,15 @@ import android.widget.Toast;
  * ，你的视图只能已最小高度进入，只有当滚动视图到达顶部时才扩 大到完整高度。
  * exitUntilCollapsed：当标题栏要往上逐渐“消逝”时，会一直往上滑动，直到剩下的的高度达到它的最小高度，
  * 再响应滑动组件的内部滑动事件。
- *
+ * <p>
  * 因为CoordinateLayout是FrameLayout，控制NestedScrollView在AppbarLayout的下面：
  * 设置标记app:layout_behavior="@string/appbar_scrolling_view_behavior"
  * 这个属性可以让NestedScrollView显示在AppBarLayout的下面
- *
+ * <p>
  * toolbar的使用：
  * Theme.AppCompat.Light.NoActionBar
  * Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
  * setSupportActionBar(toolbar);
- *
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_with_toolbar);
+        setContentView(R.layout.activity_main_with_colleaselayout);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -52,14 +54,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "NavigationOnClickListenern", Toast.LENGTH_SHORT).show();
             }
         });
+        //getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         //设置内容在Navigation展示时的距离，包括它的宽度计算在内
         //toolbar.setContentInsetStartWithNavigation(220);
 
         //toolbar.setTitle(null);//这种方式是不能讲title隐藏掉的
-        getSupportActionBar().setTitle(null);//这种方式可以实现
+        //getSupportActionBar().setTitle(null);//这种方式可以实现
         //toolbar.setLogo(R.mipmap.ic_launcher);
         //toolbar.setSubtitle("副标题");
         //toolbar.setSubtitleTextColor(Color.parseColor("#ffffff"));
 
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingtoolbarlayout);
+        collapsingToolbarLayout.setCollapsedTitleGravity(Gravity.CENTER);
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.parseColor("#ffffff"));
+        collapsingToolbarLayout.setExpandedTitleColor(Color.parseColor("#ffffff"));
     }
 }
